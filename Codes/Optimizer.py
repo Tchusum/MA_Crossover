461c0380549ffc5608605dfc7f1411ddb31ed81b
Alexandre Chisholm <alexandrechisholm08@gmail.com>
2025-02-02 10:10:37 -0500

    Streamlit done

--- Codes/Optimizer.py
+++ Codes/Optimizer.py
@@ -3,10 +3,10 @@ import Utils as utils
 import MA_Backtest as MA
 from pathlib import Path
 
-def main():
+def main(CUTOFF_DATE, RF, CAPITAL, WINDOW_SIZE_MACD_ST, WINDOW_SIZE_MACD_LT, WINDOW_SIZE_MACD_ST_RANGE, WINDOW_SIZE_MACD_LT_RANGE):
 
     #Dates
-    dates_class = utils.Dates(YEARS)
+    dates_class = utils.Dates
     today = dates_class.today_str()
 
     #Paths
@@ -20,34 +20,36 @@ def main():
     data = utils.read_csv(path)
 
     #Format date
-    data = yf_data.format_utf8_to_date(data)
+    data = utils.Dates.format_col_to_date(data, "Date")
 
     #Split data training / validation
-    training_data, validation_data = yf_data.split_data_training_validation(data, CUTOFF_DATE)
+    training_data, validation_data = yf_data.data.split_data_training_validation(data, CUTOFF_DATE)
 
     #Import FX
     path = f"{INPUT_DAILY}/FX_{today}.csv"
     fx_data = utils.read_csv(path)
-    fx_data = dates_class.format_int64_to_date(fx_data)
+    fx_data = utils.Dates.format_col_to_date(fx_data, "Date")
 
     #Mapping FX
     path = f"{INPUT_MAPPING}/Exchange_Currency.csv"
     mapping_fx = utils.read_csv(path)
 
-    tester = MA.MA_Backtester(fx_data, mapping_fx, training_data, validation_data, CAPITAL, WINDOW_SIZE_MACD_ST, WINDOW_SIZE_MACD_LT, RF)
-    tester.set_parameters(window_size_macd_st = WINDOW_SIZE_MACD_ST, window_size_macd_lt = WINDOW_SIZE_MACD_LT)
+    #base_functions = MA.Base_Functions(fx_data, mapping_fx)
+    macd_backtest = MA.MA_Backtester(fx_data, mapping_fx, training_data, validation_data, CAPITAL, WINDOW_SIZE_MACD_ST, WINDOW_SIZE_MACD_LT, RF)
+    macd_backtest.set_parameters(window_size_macd_st = WINDOW_SIZE_MACD_ST, window_size_macd_lt = WINDOW_SIZE_MACD_LT)
 
-    best_comb = tester.optimize_parameters(WINDOW_SIZE_MACD_ST_RANGE, WINDOW_SIZE_MACD_LT_RANGE, training_data)
+    best_comb = macd_backtest.optimize_parameters(WINDOW_SIZE_MACD_ST_RANGE, WINDOW_SIZE_MACD_LT_RANGE, training_data)
 
     #Export Data
     path = f"{OUTPUT}/best_comb.csv"
     utils.export_df_csv(best_comb, path)
 
+    return best_comb
+
 if __name__ == "__main__":
 
     #####Define Variables#####
     CUTOFF_DATE = '2023-01-01' #Cut-off date for training data
-    YEARS = 15 #Number of years of data to import
 
     RF = 0.01
     CAPITAL = 300
@@ -58,4 +60,4 @@ if __name__ == "__main__":
     WINDOW_SIZE_MACD_ST_RANGE = range(10, 30, 5)
     WINDOW_SIZE_MACD_LT_RANGE = range(30, 100, 5)
 
-    main(CUTOFF_DATE, YEARS, RF, CAPITAL, WINDOW_SIZE_MACD_ST, WINDOW_SIZE_MACD_LT, WINDOW_SIZE_MACD_ST_RANGE, WINDOW_SIZE_MACD_LT_RANGE)
+    main(CUTOFF_DATE, RF, CAPITAL, WINDOW_SIZE_MACD_ST, WINDOW_SIZE_MACD_LT, WINDOW_SIZE_MACD_ST_RANGE, WINDOW_SIZE_MACD_LT_RANGE)
--- Codes/Streamlit/pages/03_Optimizer.py
+++ Codes/Streamlit/pages/03_Optimizer.py
@@ -1,9 +1,30 @@
-import streamlit
+import streamlit as st
+from pathlib import Path
+import sys
 
-def app():
-    streamlit.title('Optimize Short and Long Window')
+PATH_PYTHON = Path(__file__).resolve().parent.parent.parent
+sys.path.append(str(PATH_PYTHON))
+import Import_Yfinance as yf_data
+import Utils as utils
+import MA_Backtest as MA
+import Optimizer as opt
 
-    if st.button("Run Optimization"):
-        st.write("Optimization Completed")
+WINDOW_SIZE_MACD_ST = st.session_state["WINDOW_SIZE_MACD_ST"] 
+WINDOW_SIZE_MACD_LT = st.session_state["WINDOW_SIZE_MACD_LT"]
+WINDOW_SIZE_MACD_ST_RANGE = range(10, 30, 5)
+WINDOW_SIZE_MACD_LT_RANGE = range(30, 100, 5)
+YEARS = st.session_state["YEARS"]
+CUTOFF_DATE = st.session_state["CUTOFF_DATE"]
+RF = st.session_state["RF"]
+CAPITAL = st.session_state["CAPITAL"]
 
-app()
+
+st.title('Optimize Short and Long Window')
+
+if st.button("Run Optimization"):
+
+    best_comb = opt.main(CUTOFF_DATE, RF, CAPITAL, WINDOW_SIZE_MACD_ST, WINDOW_SIZE_MACD_LT, WINDOW_SIZE_MACD_ST_RANGE, WINDOW_SIZE_MACD_LT_RANGE)
+
+    st.subheader("Best Combination")
+
+    st.dataframe(best_comb)
